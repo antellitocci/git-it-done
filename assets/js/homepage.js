950ae34b1ddea46c775553486eb926cf728d3dad
raddymaster <antellitocci@gmail.com>
2021-05-10 21:03:00 -0400

    initial github username search functionality

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,21 @@
+var userFormElem = document.querySelector("#user-form");
+var nameInputElem = document.querySelector("#username");
+
+var formSubmitHandler = function(event)
+{
+    event.preventDefault();
+    var username = nameInputElem.value.trim();
+
+    if(username){
+        getUserRepos(username);
+        nameInputElem.value ='';
+    }
+    else
+    {
+        alert("Please enter a GitHub username");
+    }
+};
+
 var getUserRepos = function(user) 
 {
     //format github api url
@@ -11,4 +29,4 @@ var getUserRepos = function(user)
     });
 };
 
-getUserRepos("facebook");
+userFormElem.addEventListener("submit", formSubmitHandler);
